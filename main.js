5e5f5ab00efe3088ebaca73e1ad422d064f6b26a
Сергей Захарян <105631225+SergeiZaharyan@users.noreply.github.com>
2023-01-17 13:37:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ const outputOrders = document.querySelector('.output-orders');
 buttonOrders.addEventListener('click',() => {
    
      outputOrders.innerHTML = textareaOrders.value.split('\n').filter(currentValue => currentValue.includes('true') || currentValue.includes('Дата вылета') || currentValue.includes('Количество туристов') || currentValue.includes('Name') || currentValue.includes('Phone') || currentValue.includes('rating') || currentValue.includes('Количество ночей')).join('<br>');
+     textareaOrders.value = "";
 })
 
