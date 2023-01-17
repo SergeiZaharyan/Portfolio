b51a72cc9c80b0398b51c6249b0da47b6a52888c
Сергей Захарян <105631225+SergeiZaharyan@users.noreply.github.com>
2023-01-17 13:31:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const textareaOrders = document.querySelector('.textarea-orders');
+const buttonOrders = document.querySelector('.button-orders');
+const outputOrders = document.querySelector('.output-orders');
+
+buttonOrders.addEventListener('click',() => {
+   
+     outputOrders.innerHTML = textareaOrders.value.split('\n').filter(currentValue => currentValue.includes('true') || currentValue.includes('Дата вылета') || currentValue.includes('Количество туристов') || currentValue.includes('Name') || currentValue.includes('Phone') || currentValue.includes('rating') || currentValue.includes('Количество ночей')).join('<br>');
+})
+
