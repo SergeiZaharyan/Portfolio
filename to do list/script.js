5101fca184365a1a8747a25ee37b5be677a23c81
Zergei Zaharyan <ssergeizaharyan96@gmail.com>
2023-06-05 18:59:37 +0200

    update

--- to do list/script.js
+++ to do list/script.js
@@ -7,41 +7,37 @@ const inputTaskAdd = document.querySelector(".conteiner-input_input");
 const listContainerBody = document.querySelector(".list-container_body");
 let containerTask = [];
 let i = 0;
- function createCard (id, text, state) {
-
-    const div = document.createElement("div");
-    if(state === "not done"){
-    div.className = "list-container_body__task-cards";}
-    else{
-        div.className = "task-cards_btn-done__green";
-    };
-    div.setAttribute("data-id", Date.now())
-    const p = document.createElement("p");
-    p.innerHTML = text;
-    const btnDone = document.createElement("button");
-   
-    const btnClean = document.createElement("button");
+function createCard(id, text, state) {
+  const div = document.createElement("div");
+  if (state === "not done") {
     div.className = "list-container_body__task-cards";
-    btnClean.className = "task-cards_btn-clean";
-    btnClean.innerHTML = "X"
-    btnDone.className = "task-cards_btn-done";
-btnDone.innerHTML = "ok";
-div.append(btnDone, p, btnClean);
-listContainerBody.append(div);
-const btnDoneCard = document.querySelector(".task-cards_btn-done");
-btnDoneCard.addEventListener("click", () =>{
-    console.log(btnDoneCard.parentElement.dataset.id)
-isDoneCard()});
+  } else {
+    div.className = "task-cards_btn-done__green";
+  }
+  div.setAttribute("data-id", Date.now());
+  const p = document.createElement("p");
+  p.innerHTML = text;
+  const btnDone = document.createElement("button");
 
-btnClean.querySelector("click", () =>{
+  const btnClean = document.createElement("button");
+  div.className = "list-container_body__task-cards";
+  btnClean.className = "task-cards_btn-clean";
+  btnClean.innerHTML = "X";
+  btnDone.className = "task-cards_btn-done";
+  btnDone.innerHTML = "ok";
+  div.append(btnDone, p, btnClean);
+  listContainerBody.append(div);
+  const btnDoneCard = document.querySelector(".task-cards_btn-done");
+  btnDoneCard.addEventListener("click", () => {
+    console.log(btnDoneCard.parentElement.dataset.id);
+    isDoneCard();
+  });
 
-
-})
-
- }
- function isDoneCard (){
-     console.log("done");
- }
- createCard ();
- createCard ()
- createCard ()
+  btnClean.querySelector("click", () => {});
+}
+function isDoneCard() {
+  console.log("done");
+}
+createCard();
+createCard();
+createCard();
