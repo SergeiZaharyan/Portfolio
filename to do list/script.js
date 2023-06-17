6f05edebc2180787a418dde1138b42de25571c31
Zergei Zaharyan <ssergeizaharyan96@gmail.com>
2023-06-17 11:18:50 +0200

    update

--- to do list/script.js
+++ to do list/script.js
@@ -5,16 +5,27 @@ const btnAddTask = document.querySelector(".conteiner-input_btn-add");
 const btnCleanAll = document.querySelector(".conteiner-input_btn-clean");
 const inputTaskAdd = document.querySelector(".conteiner-input_input");
 const listContainerBody = document.querySelector(".list-container_body");
-let containerTask = [];
-let i = 0;
+let containerTask = new Array();
+
+function generateDOM() {
+  listContainerBody.innerHTML = "";
+  containerTask.push({
+    id: Date.now(),
+    text: inputTaskAdd.value,
+    state: "not done",
+  });
+
+  containerTask.map((card) => {
+    createCard(card.id, card.text, card.state);
+  });
+
+  inputTaskAdd.value = "";
+  isSetItem();
+}
 function createCard(id, text, state) {
   const div = document.createElement("div");
-  if (state === "not done") {
-    div.className = "list-container_body__task-cards";
-  } else {
-    div.className = "task-cards_btn-done__green";
-  }
-  div.setAttribute("data-id", Date.now());
+
+  div.setAttribute("data-id", id);
   const p = document.createElement("p");
   p.innerHTML = text;
   const btnDone = document.createElement("button");
@@ -25,19 +36,94 @@ function createCard(id, text, state) {
   btnClean.innerHTML = "X";
   btnDone.className = "task-cards_btn-done";
   btnDone.innerHTML = "ok";
+  if (state === "not done") {
+    div.className = "list-container_body__task-cards";
+  } else {
+    div.className =
+      "list-container_body__task-cards task-cards_btn-done__green";
+    btnDone.innerHTML = "done";
+  }
   div.append(btnDone, p, btnClean);
   listContainerBody.append(div);
-  const btnDoneCard = document.querySelector(".task-cards_btn-done");
-  btnDoneCard.addEventListener("click", () => {
-    console.log(btnDoneCard.parentElement.dataset.id);
-    isDoneCard();
+  //   const btnDoneCard = btnDone.querySelector(".task-cards_btn-done");
+  btnDone.addEventListener("click", () => {
+    console.log(btnDone.parentElement.dataset.id, btnDone.parentElement);
+    isDoneCard(btnDone.parentElement, btnDone.parentElement.dataset.id);
   });
 
-  btnClean.querySelector("click", () => {});
+  btnClean.addEventListener("click", () => {
+    isCleanCard(btnClean.parentElement, btnClean.parentElement.dataset.id);
+  });
 }
-function isDoneCard() {
-  console.log("done");
+function isCleanCard(elementClean, elemId) {
+  console.log(elementClean.dataset.id);
+  let cardIdInArr;
+  containerTask.find((elem, index) => {
+    if (elem.id === Number(elemId)) {
+      return (cardIdInArr = index);
+    }
+  });
+  console.log(cardIdInArr);
+  console.log(elementClean.parentElement);
+  console.log(
+    listContainerBody.querySelector(`div[data-id="${elementClean.dataset.id}"]`)
+  );
+  listContainerBody
+    .querySelector(`div[data-id="${elementClean.dataset.id}"]`)
+    .remove();
+  containerTask.splice(cardIdInArr, 1);
+  console.log(containerTask);
+
+  listContainerBody.innerHTML = "";
+  containerTask.map((card) => {
+    createCard(card.id, card.text, card.state);
+    isSetItem();
+  });
 }
-createCard();
-createCard();
-createCard();
+function isDoneCard(elementDone, elemId) {
+  let cardIdInArr;
+  containerTask.find((elem, index) => {
+    if (elem.id === Number(elemId)) {
+      return (cardIdInArr = index);
+    }
+  });
+  elementDone.classList.toggle("task-cards_btn-done__green");
+  const cardText = elementDone.querySelector(`p`);
+  cardText.classList.toggle("task-cards_task-done");
+  console.log(cardIdInArr);
+  containerTask[cardIdInArr].state === "done"
+    ? (containerTask[cardIdInArr].state = "not done")
+    : (containerTask[cardIdInArr].state = "done");
+  console.log(containerTask);
+  containerTask[cardIdInArr].state === "done"
+    ? (elementDone.querySelector(`button`).innerHTML = "done")
+    : (elementDone.querySelector(`button`).innerHTML = "ok");
+  isSetItem();
+}
+function isSetItem() {
+  localStorage.setItem("arr", JSON.stringify(containerTask));
+  console.log(localStorage.getItem("arr"));
+  console.log(containerTask);
+}
+function isGetItem() {
+  const LS = localStorage.getItem("arr");
+  const localArray = JSON.parse(LS);
+  if (localArray !== containerTask) {
+    localArray ? (containerTask = localArray) : false;
+    console.log(containerTask);
+    containerTask
+      .map((card) => {
+        createCard(card.id, card.text, card.state);
+      })
+      .join("");
+  }
+}
+btnAddTask.addEventListener("click", () => {
+  generateDOM();
+});
+btnCleanAll.addEventListener("click", () => {
+  listContainerBody.innerHTML = "";
+  containerTask = [];
+  isSetItem();
+});
+isGetItem();
